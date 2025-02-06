4fccd619c446963a2f71680fdc1733c9a2d4d7c7
Fran <francanocor@gmail.com>
2025-02-06 20:51:54 +0000

    Primer commit

---
+++ src/main/java/com/mycompany/compartido/Compartido.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compartido;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Compartido {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
